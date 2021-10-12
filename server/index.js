d3c1394cebe736b3658b2542c97fc99e03b5c2c1
KevinG <ginta.kevin@gmail.com>
2021-10-12 15:58:37 -0700

    updates

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     next();
-  });
+});
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
