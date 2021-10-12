178ea15aad973deb2949eb9666b66691ee1fbf8e
KevinG <ginta.kevin@gmail.com>
2021-10-12 15:43:17 -0700

    update

--- server/index.js
+++ server/index.js
@@ -11,6 +11,14 @@ dotenv.config();
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  });
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
